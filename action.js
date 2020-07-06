247b384677038d486c287ac1fb125d19484d33fd
Annie Paul <annie.paul.668@gmail.com>
2020-07-06 17:18:04 -0500

    my recent activity savespast responses with js arrays

--- action.js
+++ action.js
@@ -13,3 +13,57 @@ function showInput()
 			// document.getElementById("fl2").innerHTML = "Rating (1-5): " + document.myform.rating.value;
 
 }
+
+// tryna make an array that stores song name
+// LEFT OFF HERE USE THE WEBSITE AND TRY TO GET THIS ARRAY TO WORK YEET
+var songIndex     = 0;
+var commentIndex  = 0;
+var ratingIndex   = 0;
+var songNameArray = Array();
+var commentArray  = Array();
+var ratingArray   = Array();
+
+function addElementToSongNameArray() {
+	songNameArray[songIndex] = document.myform.input.value;
+	//alert("Element: " + songNameArray[songIndex] + " added at index " + songIndex);
+	songIndex++;
+	document.getElementById("results1").value = "";
+}
+
+function addElementToCommentArray() {
+	commentArray[commentIndex] = document.myform.comment.value;
+	//alert("Element: " + commentArray[commentIndex] + " added at index " + commentIndex);
+	commentIndex++;
+	document.getElementById("results2").value = "";
+}
+
+function addElementToRatingArray() {
+	ratingArray[ratingIndex] = document.myform.rating.value;
+	//alert("Element: " + ratingArray[ratingIndex] + " added at index " + ratingIndex);
+	ratingIndex++;
+	document.getElementById("results3").value = "";
+}
+
+function displaySongNameArray() {
+	var output = "";
+	for (var i = 0; i < songNameArray.length; i++) {
+		output +=  "Element " + i + " = " + songNameArray[i] + "<br/>";
+	}
+	document.getElementById("results1").innerHTML = output;
+}
+
+function displayCommentArray() {
+	var output = "";
+	for (var i = 0; i < commentArray.length; i++) {
+		output +=  "Element " + i + " = " + commentArray[i] + "<br/>";
+	}
+	document.getElementById("results2").innerHTML = output;
+}
+
+function displayRatingArray() {
+	var output = "";
+	for (var i = 0; i < ratingArray.length; i++) {
+		output +=  "Element " + i + " = " + ratingArray[i] + "<br/>";
+	}
+	document.getElementById("results3").innerHTML = output;
+}
